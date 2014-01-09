45c6160b296482f5a4b4824e7847c80b3b9a966d
Jacek Marek <jmarek@antennasoftware.com>
2014-01-09 16:20:51 +0100

    rewrite test for ReadSqlCeCommandsFromString to use sample data

--- CityCommon.Test/CommonUtilitiesTest.cs
+++ CityCommon.Test/CommonUtilitiesTest.cs
@@ -1,22 +1,29 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace City.Test
+﻿namespace City.Test
 {
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class CommonUtilitiesTest
     {
+        private const string FirstStatement = @"ALTER TABLE [CreditCards] DROP CONSTRAINT [FK_CreditCardPerson];";
+
+        private const string SecondStatement = @"ALTER TABLE [Transactions] DROP CONSTRAINT [FK_CreditCardTransactions];";
+
+        private const string ThirdStatement = @"ALTER TABLE [Transactions] DROP CONSTRAINT [FK_PoiTransaction];";
+
+        private const string ThreeStatementsBlockString =
+            "\r\n    ALTER TABLE [CreditCards] DROP CONSTRAINT [FK_CreditCardPerson];\r\nGO\r\n    ALTER TABLE [Transactions] DROP CONSTRAINT [FK_CreditCardTransactions];\r\nGO\r\n    ALTER TABLE [Transactions] DROP CONSTRAINT [FK_PoiTransaction];";
+
         [TestMethod]
-        public void ReadCommandsTest()
+        public void ReadSqlCeCommandsFromString_GivenMultipleCommands_ExtractsThemToEnumerable()
         {
-            var text = @"D:\test.sqlce";
+            var actualCommands = CommonUtilities.ReadSqlCeCommandsFromString(ThreeStatementsBlockString).ToList();
 
-            var commands = CommonUtilities.ReadSqlCeCommandsFromString(File.ReadAllText(text));
-
+            Assert.Equals(3, actualCommands.Count);
+            Assert.Equals(FirstStatement, actualCommands[0]);
+            Assert.Equals(SecondStatement, actualCommands[1]);
+            Assert.Equals(ThirdStatement, actualCommands[2]);
         }
     }
 }
